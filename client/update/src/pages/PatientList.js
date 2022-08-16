1bc57047ac5d4306887104bc5242c1e962d54e5b
AnishNandhan <anishnandhan03@gmail.com>
2022-08-16 21:43:52 +0530

    testing

--- client/update/src/pages/PatientList.js
+++ client/update/src/pages/PatientList.js
@@ -4,15 +4,18 @@ import { PatientContext } from '../contexts/PatientContext'
 
 const PatientList = () => {
     const [patients, setPatients] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         const fetchPatients = async () => {
+            setLoading(true)
             const response = await fetch('/api/patients');
             const json = await response.json();
 
             if (response.ok) {
                 console.log(json);
                 setPatients(json);
+                setLoading(false);
             }
         }
         fetchPatients();
@@ -20,6 +23,9 @@ const PatientList = () => {
 
   return (
     <div className='container patient-list'>
+        { loading ? (
+            <p>Loading....</p>
+        ) : 
         {patients && patients.map((patient) => (
             <div className='patient' key={patient._id}>
                 <div className='patient-short-details'>
@@ -35,7 +41,10 @@ const PatientList = () => {
                     <button>Delete</button>
                 </div>
             </div>
-        ))}
+        ))
+        }
+        {'}'}
+        
     </div>
   )
 }
